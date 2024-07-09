e4c4b1458b74e0ca52ef2ca394d4331ec8a46dc8
diego-dc <diegcavi@hotmail.com>
2024-07-09 12:10:39 -0400

    deploy test 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
+  basePath: isProd ? '/diego-dc.github.io' : '',
+  assetPrefix: isProd ? '/diego-dc.github.io/' : '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
